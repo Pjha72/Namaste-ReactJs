fc21fb63a96e3b6fcf82d1b5ecc70ec546cbea9a
Pjha72 <pjha76737@gmail.com>
2024-01-11 23:53:51 +0530

    New Commit

--- Lec-04/Note.md
+++ Lec-04/Note.md
@@ -15,5 +15,36 @@ Ans : git init
 babel plugin transform remove console
 * Link  : `https://babeljs.io/docs/babel-plugin-transform-remove-console/`
 
+6. React Reconciliation : https://legacy.reactjs.org/docs/reconciliation.html
+
+7. render : something updating in the DOM
+
+8. JSX is like HTML inside the JavaScript. This Statement is not True. 
+  * That Code is looks like a HTML Syntax.
+
+9. Difference Between HTML & JSX?
+
+10. How does JSX Execute?
+Ans : Babel is execute the JSX Syntax.
+     * Babel is a JavaScript Compiler.
+     * Link : https://babeljs.io/
+     * https://github.com/babel/babel/blob/main/CONTRIBUTING.md
+
+11. What are the Different Uses of JSX?
+Ans : 
+
+12. Advantge of JSX
+   * Readbility
+   * Less Code
+   * Maintaibility
+
+13. JSX is very Secure
+14. Component composition is the name for passing components as props to other components.
+* https://felixgerschau.com/react-component-composition/
+
+15. Is the Component Hoisted?
+Ans : Yes, the component is hoisted in React. Hoisting is a JavaScript mechanism that moves all variable and function declarations to the top of their scope. This means that you can use a component before you have declared it.
+For example, you can create a component called App and then use it in another component called MyComponent before you have declared App. This is because the App component will be hoisted to the top of its scope.
+
 
 
--- Lec-04/index.js
+++ Lec-04/index.js
@@ -1,23 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-    "h1",
-    {
-        id: "title"
-    },
-    "heading" 
-);
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id: "title",
+//         key: "h1"
+//     },
+//     "Namaste React" 
+// );
+// console.log(heading);
 
-const heading1 = React.createElement(
-    "h1",{id:"title"},"heading1"
-);
+// React.createElement => gives a object ==> HTML(DOM)
+// const heading1 = React.createElement(
+//     "h1",{id:"title", key:"h2"},"heading1"
+// );
 
-const container = React.createElement(
-    "div",{id:"container"},[heading,heading1]
+
+/**
+ <div class="header">
+        <h1>Namaste React</h1>
+        <ul>
+            <li>About US</li>
+            <li>Support</li>
+            <li>Home</li>
+        </ul>
+    </div>
+   
+    Convert These HTML into React 
+ */
+// const ordereList1 = React.createElement("li",{},"About US");
+// const ordereList2 = React.createElement("li",{},"Support");
+// const ordereList3 = React.createElement("li",{},"Home");
+// const unorderedList = React.createElement(
+//     "ul",{},[ordereList1,ordereList2,ordereList3]
+// );
+
+// const container = React.createElement(
+//     "div",{id:"container"},[heading,unorderedList]
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(container);
+
+/********* JSX : JavaScript XML ***********/
+/**
+ * JSX Uses Behind the Scene React.createELement=> Object=>HTML(DOM)
+ * 
+ */
+const Head = () => (
+    <h1 id='title' key="h2" tabIndex="1" className='Apna'>
+        Namaste React
+    </h1>
 );
+// console.log(head); // Object
+
+
+/******* 
+ * Types of React Component 
+ * 1. Functional Component : New way of writing the code
+ *    1.1 : Functional Component is nothing it's a javascript
+ *          function which return JSX.
+ *    1.2 : Name of the component should be starts with captial letter Not Mandatory.
+ *    1.3 : Functional Component is a Normal Function.
+ *    1.4 : If you write the functional component just like a
+ *          normal variable like a `{Head()}`
+ * 2. Class Based Component : Old way of writing the code
+ * 
+ *******/
+// Example of Functional Component
+const HeaderComponent = () => {
+    // return <h1>Namaste React Functional Component</h1>;
+    return (
+        <div>
+            {/* 1st Way */}
+            {/* Component Composition OR Composing Component */}
+            {/* <Head /> */}
+
+            {/* 2nd Way */}
+            {Head()}
+            
+            <h1>Namaste React Functional Component</h1>
+            <h1>This is a second h1 tag</h1>
+        </div>
+    )
+};
+// Other Ways to write the Functional Component
+// const HeaderComponent1 = () => (
+//     <div>
+//         <h1>Namaste React Functional Component</h1>
+//         <h1>This is a second h1 tag</h1>
+//     </div>
+// );
+// console.log(HeaderComponent);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+/** When render the React element Used This Syntax */
+// root.render(head);
 
-
+/** When Render the React Component */
+root.render(<HeaderComponent />);
